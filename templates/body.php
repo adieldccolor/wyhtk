5de52610ebed709107f0f0aa5eb54c35c37be30e
Rolando Arias <rolando@dccolorweb.com>
2014-10-24 14:50:24 -0600

    Update Project

---
+++ templates/body.php
@@ -0,0 +1,8 @@
+<?php
+include("body-navbar.html");
+include("body-hero.html");
+include("body-hosters.html");
+include("body-location.html");
+include("body-partners.html");
+include("body-footer.html");
+?>
---
+++ templates/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WYNTK</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
