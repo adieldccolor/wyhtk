2cb00d7c916e180b1b0f27765d36dfba015eb5a3
Rolando Arias <rolando@dccolorweb.com>
2014-10-29 17:24:52 -0600

    Agregando body-partners

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,10 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$('.carousel').carousel();	
+		});
+	</script>
 </body>
 </html>
