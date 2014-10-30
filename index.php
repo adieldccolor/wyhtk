f6dd31583376b68bd4429d1d7a2be063bc0ac8f8
Rolando Arias <rolando@dccolorweb.com>
2014-10-30 09:28:29 -0600

    Actualizar Index

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<a href="wyntk-01.php">wyntk-01</a><br>
-<a href="wyntk-02.php">wyntk-02</a><br>
+<?php
+include 'templates/header.php';
+include 'templates/body2.php';
+include 'templates/footer.php';
+?>
