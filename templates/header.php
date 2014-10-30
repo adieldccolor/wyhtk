e3d6cc7832ffaf8f84f5dc19d510a4b158b73969
adielhercules <adiel@dccolorweb.com>
2014-10-30 16:41:49 -0600

    new viewport

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>WYNTK</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="maximum-scale=1.0,width=device-width,initial-scale=1.0,user-scalable=0">
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/style.css">
