00052d08e2a3a54841c08578a239cbd78eea4330
adielhercules <adiel@dccolorweb.com>
2014-10-30 15:59:01 -0600

    update cover backgrounds

--- templates/footer.php
+++ templates/footer.php
@@ -2,6 +2,7 @@
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="assets/js/bootstrap.min.js"></script>
 	<script src="assets/js/animatescroll.min.js"></script>
+	<script src="assets/js/jquery.backstretch.min.js"></script>
 	<script src="assets/js/wheel-mainFunctions.js"></script>
 </body>
 </html>
