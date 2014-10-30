9d828eab645ee12b0535128b124f4114259e217f
Rolando Arias <rolando@dccolorweb.com>
2014-10-29 19:28:55 -0600

    Actualizando

--- index.php
+++ index.php
@@ -1,5 +1,2 @@
-<?php
-include 'templates/header.php';
-include 'templates/body.php';
-include 'templates/footer.php';
-?>
+<a href="wyntk-01.php">wyntk-01</a><br>
+<a href="wyntk-02.php">wyntk-02</a><br>
